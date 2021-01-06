763a6cb733ddb98c27c7a8f43ca3d6fb8d6b9119
JimmyVdEynden <jimmy.vandeneynden@ugent.be>
2021-01-06 13:20:58 +0100

    revision

---
+++ scripts/downloads/download_CCLE.sh
@@ -0,0 +1,7 @@
+# Manually downloaded 
+# Mutation data from https://ndownloader.figshare.com/files/25494419
+# Sample informtion data from https://ndownloader.figshare.com/files/25494443
+
+# From TRON: HLA genotypes
+wget http://celllines.tron-mainz.de/media/TRON_seq2hla.csv.gz
+gunzip TRON_seq2hla.csv.gz
--- scripts/downloads/get_download_scripts.sh
+++ scripts/downloads/get_download_scripts.sh
@@ -6,3 +6,4 @@ cp downloads/TCGA/maf/mc3/download_process.R scripts/downloads/download_mc3.R
 cp downloads/cosmic/CGC/v91/download_process.R scripts/downloads/download_cgc.R
 cp downloads/TCGA/clin/download_process.R scripts/downloads/download_TCGA_id.R 
 cp downloads/1kg/download.sh scripts/downloads/download_1kg_HLA_alleles.sh
+cp downloads/CCLE/download.sh scripts/downloads/download_CCLE.sh
